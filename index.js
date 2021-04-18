34b0f57c7908b9c8b0d22eb63cef5b466775dfa0
hendriksandy <79459594+hendriksandy@users.noreply.github.com>
2021-04-19 00:36:15 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const age = 17;
+const isFemale = true;
+const driverStatus = "sjaak";
+
+if (age >= 18) {
+    console.log("je mag naar binnen")
+
+} else {
+    console.log("je mag niet naar binnen")
+}
+
+if (isFemale) {
+    console.log("je bent een vrouw")
+} else {
+    console.log("je bent geen vrouw")
+}
+
+
+if (driverStatus == "bob") {
+    console.log("je bent de bob en mag rijden")
+} else {
+    console.log("je bent een zuiplap en mag niet meer autorijden")
+}
